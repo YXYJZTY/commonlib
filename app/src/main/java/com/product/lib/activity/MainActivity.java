ebc4f7f953d8777857f8a3b51bf4a977b4ebbfab
2411320454@qq.com <2411320454@qq.com>
2018-11-09 15:57:23 +0800

    添加config文件配置

---
+++ app/src/main/java/com/product/lib/activity/MainActivity.java
@@ -0,0 +1,17 @@
+package com.product.lib.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.product.lib.R;
+import com.socks.library.KLog;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
---
+++ app/src/main/java/com/product/lib/app/App.java
@@ -0,0 +1,14 @@
+package com.product.lib.app;
+
+import android.app.Application;
+
+
+/**
+ * Author  : xiaohei
+ * Date   : 2018/11/9  13:39
+ * Description : App
+ */
+public class App extends Application{
+
+
+}
